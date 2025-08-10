c96d26b4cd93d67b574e734f666608ac6d0342a9
George Jones <georgekeesjones@gmail.com>
2025-08-10 16:37:39 +0300

    end

--- class-code-controlled-forms/src/App.jsx
+++ class-code-controlled-forms/src/App.jsx
@@ -1,21 +1,32 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [formData, setFormData] = useState({})
+  const [formData, setFormData] = useState({
+    firstName: '',
+    city: ''
+  })
 
   const handleChange = (event) => {
     console.log('Change handler has run!')
     // update formdata.x with the value of x
-    setFormData({...formData, [event.target.name]: event.target.value})
-    console.log(formData)
+    // setFormData({...formData, [event.target.name]: event.target.value})
+    const currentFormData = {...formData}
+    
+    const inputName = event.target.name
+    const inputValue = event.target.value
+    
+    currentFormData[inputName] = inputValue
+    
+    setFormData(currentFormData)
+    // console.log(formData)
   }
 
 
   return (
     <>
       <h1>Controlled Forms</h1>
-      <input name='city' onChange={handleChange}></input>
-      <input name='firstName' onChange={handleChange}></input>
+      <input value={formData.city} name='city' onChange={handleChange}></input>
+      <input value={formData.firstName} name='firstName' onChange={handleChange}></input>
     </>
   )
 }
